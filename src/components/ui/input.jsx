9c968b20bf221227362be3cd5aaf355ba9eb3fca
Cafeinado-pixel <curadorbebidas@gmail.com>
2025-07-30 22:48:39 -0300

    Delete src directory

--- src/components/ui/input.jsx
+++
@@ -1,8 +0,0 @@
-export function Input({ className = '', ...props }) {
-  return (
-    <input
-      className={`border rounded-lg px-3 py-2 w-full shadow focus:outline-none focus:ring-2 focus:ring-blue-300 ${className}`}
-      {...props}
-    />
-  );
-}
